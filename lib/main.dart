a712bca1c0714b304a06cadef8a806cd6f891995
randiltennakoon <randil2016al@gmail.com>
2021-03-03 19:05:53 +0530

    updated widget colors

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF1B3352),
-        scaffoldBackgroundColor: Color(0xFF112131),
+      theme: ThemeData(
+        // primaryColor: Color(0xFF1B3352),
+        // scaffoldBackgroundColor: Color(0xFF112131),
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Color(0xFFF4F7FD),
       ),
       home: UserInputPage(),
     );
